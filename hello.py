8fb091b63ae9d82b98e352f798632ccaf8223dee
Vane <dadou19930929@126.com>
2017-12-20 15:33:58 +0800

    Day1

---
+++ hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+print('hello,Vane')
+
+print('hello','Vane','你好')
+
+print(100)
+
+print(100+200)
+
+print('100+200 =',100+200)
+
+# bianchang = int(input('请输入正方形的边长'))
+
+# print('正方形的面积是',bianchang*bianchang)
+    
+print('I\'m \"OK\"')
+
+print('I\'m \n\"OK\"')
+
+# r'' 不转义
+print(r'I\'m \n\"OK\"')
+
